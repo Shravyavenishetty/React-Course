cb8094488cb85d81a12f5ea7fb723aff951be07f
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-09-23 17:31:03 +0530

    added Link tag instead of anchor tag

--- ecommerce-project/src/pages/TrackingPage.jsx
+++ ecommerce-project/src/pages/TrackingPage.jsx
@@ -1,5 +1,6 @@
 import { Header } from '../components/Header';
 import './TrackingPage.css';
+import { Link } from 'react-router';
 
 export function TrackingPage() {
     return (
@@ -9,9 +10,9 @@ export function TrackingPage() {
             <Header />
             <div class="tracking-page">
                 <div class="order-tracking">
-                    <a class="back-to-orders-link link-primary" href="/orders">
+                    <Link class="back-to-orders-link link-primary" to="/orders">
                         View all orders
-                    </a>
+                    </Link>
 
                     <div class="delivery-date">
                         Arriving on Monday, June 13
