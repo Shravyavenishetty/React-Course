6ce2f303d319ade333397efd54d0889aca8d77c6
Venishetty Shravya <shravyavenishetty03@gmail.com>
2025-09-24 20:56:42 +0530

    create DeliveryDate

--- ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,7 +1,7 @@
-import dayjs from 'dayjs';
 import { DeliveryOptions } from './DeliveryOptions';
 import axios from 'axios';
 import { CartItemDetails } from './CartItemDetails';
+import { DeliveryDate } from './DeliveryDate';
 
 export function OrderSummary({ cart, deliveryOptions, loadCart }) {
     const deleteCartItem = async (productId) => {
@@ -12,22 +12,15 @@ export function OrderSummary({ cart, deliveryOptions, loadCart }) {
 
         <div className="order-summary">
             {deliveryOptions.length > 0 && cart.map((cartItem) => {
-                const selectedDeliveryOption = deliveryOptions
-                    .find((deliveryOption) => {
-                        return deliveryOption.id === cartItem.deliveryOptionId;
-                    });
 
                 return (
                     <div key={cartItem.productId} className="cart-item-container">
-                        <div className="delivery-date">
-                            Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                        </div>
+                        <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions} />
 
                         <div className="cart-item-details-grid">
                             <CartItemDetails cartItem={cartItem} onDelete={() => deleteCartItem(cartItem.productId)} />
 
                             <DeliveryOptions deliveryOptions={deliveryOptions} cartItem={cartItem} loadCart={loadCart} />
-
                         </div>
                     </div>
                 );
